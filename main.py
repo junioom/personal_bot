eacf330bf3e315a3bc816d6561bac7c87d934c84
Junior Melo <juniorbmelo12@gmail.com>
2026-05-03 13:16:01 -0300

    personal_bot 1.3.1

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ from google.oauth2.service_account import Credentials
 from langchain.agents import AgentExecutor, create_react_agent
 from langchain.tools import Tool
 from langchain_openai import ChatOpenAI
-from langchain.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate
 
 from telegram import Update
 from telegram.ext import (
